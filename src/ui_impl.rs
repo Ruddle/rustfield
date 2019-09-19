ee6cc0d6737f6590f33762b61481d04c468ca0af
Thomas SIMON <thomas.simon.work@gmail.com>
2019-09-19 22:09:20 +0200

    More in depth explanation

--- src/ui_impl.rs
+++ src/ui_impl.rs
@@ -1,8 +1,9 @@
 use crate::misc::Vector2;
-use ggez::event::{KeyCode, KeyMods, MouseButton};
+use ggez::event::{KeyCode, MouseButton};
 use imgui::*;
 use std::collections::HashSet;
 
+#[derive(Copy, Clone, PartialEq)]
 pub enum DisplayFlowField {
     Cost,
     Integration,
@@ -14,11 +15,9 @@ pub struct UI {
     pub compute_live: bool,
     pub compute_step: bool,
     pub compute_all: bool,
-
     pub mouse_pos: Vector2,
     pub keys_pressed: HashSet<KeyCode>,
     pub mouse_pressed: HashSet<MouseButton>,
-
     pub zoom: f32,
     pub zoom_smooth: f32,
     pub cam_pos: Vector2,
@@ -37,7 +36,6 @@ impl UI {
             mouse_pos: Vector2::new(0.0, 0.0),
             keys_pressed: HashSet::new(),
             mouse_pressed: HashSet::new(),
-
             zoom: 1.0,
             zoom_smooth: 1.0,
             cam_pos: Vector2::new(-250.0, -400.0),
@@ -53,50 +51,50 @@ impl UI {
                 .size([300.0, 400.0], imgui::Condition::FirstUseEver)
                 .position([400.0, 100.0], imgui::Condition::FirstUseEver)
                 .build(|| {
-                    ui.text(im_str!("Draw a maze"));
-                    ui.text(im_str!("Left click : Wall"));
-                    ui.text(im_str!("Right click : Erase"));
-                    ui.text(im_str!("Then click on the Display button"));
-                    ui.text(im_str!("Then click anywhere on your maze"));
-                    ui.text(im_str!("to generate the flow field"));
-                    ui.text(im_str!("leading to where you clicked"));
+                    match self.flowfield_mode{
+                        DisplayFlowField::Cost => {
+                            ui.text(im_str!("Draw a maze"));
+                            ui.bullet_text(im_str!("Left click : Wall"));
+                            ui.bullet_text(im_str!("Right click : Erase"));
+                            ui.bullet_text(im_str!("Middle click : Reset"));
+                            ui.text(im_str!("Then check the 'integration' display"));
+                        }
+                        DisplayFlowField::Integration => {
+                            ui.text(im_str!("Set a destination"));
+                            ui.bullet_text(im_str!("Left click : place target"));
+                            ui.text(im_str!("Play with the settings below"));
+                            ui.text(im_str!(""));
+                            ui.text(im_str!(""));
+                        }
+                    }
 
                     ui.separator();
                     ui.text(im_str!("Camera control: ZSQD/Scroll"));
                     ui.separator();
-                    let mouse_pos = ui.io().mouse_pos;
                     ui.text(im_str!(
                         "Mouse Position: ({:.1},{:.1})",
-                        mouse_pos[0],
-                        mouse_pos[1]
-                    ));
-
-                    ui.text(im_str!(
-                        "Mouse Position Cam: ({:.1},{:.1})",
                         self.mouse_pos_camera.x,
                         self.mouse_pos_camera.y
                     ));
                     ui.separator();
 
                     ui.text(im_str!("Display: "));
-                    ui.same_line(0.0);
-
-                    let btn_text = match self.flowfield_mode {
-                        DisplayFlowField::Cost => "Cost",
-                        DisplayFlowField::Integration => "Integration",
-                    };
-                    let next = match self.flowfield_mode {
-                        DisplayFlowField::Cost => DisplayFlowField::Integration,
-                        DisplayFlowField::Integration => DisplayFlowField::Cost,
-                    };
-
-                    if ui.small_button(&ImString::new(btn_text)) {
-                        self.flowfield_mode = next;
-                    }
+                    ui.radio_button(im_str!("Cost field"),&mut self.flowfield_mode,DisplayFlowField::Cost);
+                    ui.radio_button(im_str!("Integration field"),&mut self.flowfield_mode,DisplayFlowField::Integration);
 
                     if ui.checkbox(im_str!("Show flow arrows"), &mut self.flowfield_show_arrow) {
                         println!("check changed");
                         println!("to {}", self.flowfield_show_arrow);
+                    }
+
+                    if ui.is_item_hovered(){
+                        ui.tooltip(||
+                                       {
+                                           let tok = ui.push_text_wrap_pos(ui.get_cursor_pos()[0]+ 150.0);
+                                           ui.text(im_str!("Shows an arrow on each cell of the map indicating the shortest way to the target"));
+                                           std::mem::drop(tok);
+                                       }
+                        );
                     }
 
                     ui.checkbox(im_str!("Compute all"), &mut self.compute_all);
